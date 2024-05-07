52b08cdedccf0b63744b5ef7b772612528c7e416
KOSASIH <kosasihg88@gmail.com>
2024-05-07 14:05:50 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
+const formatDate = (date) => date.toLocaleString();
+
+const generateRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const toWei = (value, decimals) =>
+  web3.utils.toWei(value.toString(), decimals.toString());
+
+const fromWei = (value, decimals) =>
+  web3.utils.fromWei(value.toString(), decimals.toString());
+
+export { formatDate, generateRandomNumber, toWei, fromWei };
 const { ethers } = require('ethers');
 
 const toWei = (value) => ethers.utils.parseEther(value.toString());
