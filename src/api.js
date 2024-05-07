2004930e8eea34f9092c95256ae2a261cc8b8184
KOSASIH <kosasihg88@gmail.com>
2024-05-07 14:05:13 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,14 @@
+import axios from 'axios';
+
+const get = (url) => axios.get(url);
+
+const post = (url, data) => axios.post(url, data);
+
+const put = (url, data) => axios.put(url, data);
+
+const del = (url) => axios.delete(url);
+
+export { get, post, put, del };
 const express = require('express');
 const router = express.Router();
 const { getContracts, createContract } = require('../blockchain');
