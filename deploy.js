77277cc6b96d7d893bb19153385eccc424561d44
KOSASIH <kosasihg88@gmail.com>
2024-05-07 13:50:27 +0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,3 +3,32 @@ const { execSync } = require('child_process');
 execSync('npm run build');
 
 // Add additional deployment steps here as needed
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const { interface, bytecode } = require('../build/contracts/GEEPlatform.json');
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.INFURA_ENDPOINT
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(interface)
+    .deploy({
+      data: bytecode,
+    })
+    .send({
+      gas: 4000000,
+      from: accounts[0],
+    });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
