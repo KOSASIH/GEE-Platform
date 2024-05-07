8021e500cc1402c54525f36b6bd17b27588714a8
KOSASIH <kosasihg88@gmail.com>
2024-05-07 14:07:36 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,13 @@
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const NULL_BYTES = '0x';
+
+export const EMPTY_STRING = '';
+
+export const ZERO = 0;
+
+export const ONE = 1;
+
 const { toContractUnits } = require('./config');
 
 const INITIAL_SUPPLY = toContractUnits(1000000, 18);
