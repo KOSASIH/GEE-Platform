cb54af3bbeb0570d1b75dc2174dda5cfbe0ff39a
KOSASIH <kosasihg88@gmail.com>
2024-05-07 13:49:32 +0700

    Update compile.js

--- compile.js
+++ compile.js
@@ -15,3 +15,35 @@ async function compile() {
 }
 
 compile();
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const contractPath = path.resolve(__dirname, '..', 'contracts', 'GEEPlatform.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'GEEPlatform.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['GEEPlatform.sol'];
+const contractName = Object.keys(output)[0];
+const contractData = output[contractName];
+
+fs.ensureDirSync(path.resolve(__dirname, '..', 'build', 'contracts'));
+fs.writeJsonSync(
+  path.resolve(__dirname, '..', 'build', 'contracts', contractName + '.json'),
+  contractData
+);
