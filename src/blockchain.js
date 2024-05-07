6c552c371a819aeddfd79799f96d4f98fcb19296
KOSASIH <kosasihg88@gmail.com>
2024-05-07 14:04:39 +0700

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,17 @@
+import Web3 from 'web3';
+
+const web3 = new Web3(process.env.INFURA_ENDPOINT);
+
+const getContract = (abi, address) => new web3.eth.Contract(abi, address);
+
+const sendTransaction = (transaction) =>
+  web3.eth.sendTransaction(transaction);
+
+const getBlockNumber = () => web3.eth.getBlockNumber();
+
+const getBalance = (address) => web3.eth.getBalance(address);
+
+export { getContract, sendTransaction, getBlockNumber, getBalance };
 const Web3 = require('web3');
 const { abi } = require('../build/contracts/GEEPlatform.json');
 
