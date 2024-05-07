c8552ee943f8e39d4239c146ca177c32da4876c9
KOSASIH <kosasihg88@gmail.com>
2024-05-07 13:51:52 +0700

    Update start_local.js

--- start_local.js
+++ start_local.js
@@ -5,3 +5,34 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   debug(`Listening on port ${port}...`);});
+const path = require('path');
+const fs = require('fs-extra');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('../build/contracts/GEEPlatform.json');
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  'http://localhost:8545'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(interface)
+    .deploy({
+      data: bytecode,
+    })
+    .send({
+      gas: 4000000,
+      from: accounts[0],
+    });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
