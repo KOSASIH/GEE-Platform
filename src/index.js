f321570d760a3d4fd7c1f4b8c7214d52116ebe78
KOSASIH <kosasihg88@gmail.com>
2024-05-07 14:04:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 const startApi = require('./start_api');
 
 startApi();
